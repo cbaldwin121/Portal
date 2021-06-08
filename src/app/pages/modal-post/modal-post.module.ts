1ed37a5c8e7397c3636ef94c6a3a7ff32c4efe27
Colleen <cb3567a@student.american.edu>
2021-06-08 11:40:15 -0400

    organizing new app layout

---
+++ src/app/pages/modal-post/modal-post.module.ts
@@ -0,0 +1,15 @@
+
+import { NgModule } from '@angular/core';
+import { ModalPost } from './modal-post';
+
+@NgModule({
+  declarations: [
+    ModalPost,
+  ],
+  imports: [
+  ],
+  exports: [
+    ModalPost
+  ]
+})
+export class ModalPostModule {}
